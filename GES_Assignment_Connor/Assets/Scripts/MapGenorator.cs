ce9ad5ee54b1db3682ebf6b1999a71e376e05826
Cupiditas <Ligert123@hotmail.com>
2017-05-06 10:27:36 +1000

    Update 2:
    - Objects can now be spawned, the prefab must have the"objectData"
    script added too it, the information in the script is the options for
    generation. the min and max percentage values is how far from the center
    of the path thigs can be: min 0, max 100 lets it generate anywhere on
    the path. min 50, max 60 means it can only generate past the 50% mark of
    the Side-> centre area and the furthest it can generate is 60% of that
    area...have fun with that

--- GES_Assignment_Connor/Assets/Scripts/MapGenorator.cs
+++ GES_Assignment_Connor/Assets/Scripts/MapGenorator.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 public class MapGenorator : MonoBehaviour {
     public float partWidth = 10;
     public float wallHeight = 5;
-    public GameObject pathObject;
+    public GameObject[] spawnableObjects;
+    public GameObject[] homeTree;
     public int PathAmount4 = 6;
     public int PathAmount3 = 6;
     public int PathAmount2 = 6;
     public int pathsPerSide = 2;
+    bool enemyAllowed = false;
+    bool friendlyAllowed = false;
     int path4 = 0;
     int path3 = 0;
     int path2 = 0;
@@ -40,9 +42,9 @@ public class MapGenorator : MonoBehaviour {
         int count = 0;
         int pathCount = 4;
         int pathOutput;
-        float timer;
+
         paths[currentPosR, currentPosC] = new pathObject();
-        paths[currentPosR, currentPosC].generate(currentPositionX, currentPositionZ,partWidth,wallHeight,requiredSides,maxSides, currentPosR, currentPosC);
+        paths[currentPosR, currentPosC].generate(currentPositionX, currentPositionZ,partWidth,wallHeight,requiredSides, maxSides,homeTree, false, false, "row: " + currentPosR.ToString() + " col: " + currentPosC.ToString(),gameObject);
         //currentPositionX += gridx;
         requiredSides = new int[4] { 0, 0, 0, 0 };
         while (mapGenerate == true){
@@ -73,7 +75,7 @@ public class MapGenorator : MonoBehaviour {
                 layer += 1;
                 
             }
-            if (count == (pathsPerSide * 2+1) * (pathsPerSide * 2+1)-1 || pathCount - count == 0)
+            if (count == (pathsPerSide * 2+1) * (pathsPerSide * 2+1)-1)
             {
                 mapGenerate = false;
                 break;
@@ -148,7 +150,7 @@ public class MapGenorator : MonoBehaviour {
                 requiredSides[2] = paths[currentPosR, currentPosC+1].sideNeeded(3);
             }
             paths[currentPosR, currentPosC] = new pathObject();
-            pathOutput = paths[currentPosR, currentPosC].generate(currentPositionX, currentPositionZ,partWidth,wallHeight,requiredSides,maxSides,currentPosR,currentPosC);
+            pathOutput = paths[currentPosR, currentPosC].generate(currentPositionX, currentPositionZ,partWidth,wallHeight,requiredSides,maxSides,spawnableObjects,enemyAllowed,friendlyAllowed, "row: " + currentPosR.ToString() + " col: " + currentPosC.ToString(), gameObject);
             if (pathOutput == 4)
             {
                 path4 += 1;
@@ -177,19 +179,19 @@ public class MapGenorator : MonoBehaviour {
     // Update is called once per frame
 
 }
-
 public class pathObject
 {
     bool[] _sides = new bool[4];
     float[] _position = new float[2];
     float chance;
     int sideCount = 0;
+    string pathType;
     GameObject _floor;
     GameObject[] _path1;
     GameObject[] _path2;
     GameObject[] _path3;
     GameObject[] _path4;
-    public int generate(float x, float z,float width,float wallHeight,int[] sidesRequired,int maxSides,int row,int column)
+    public int generate(float x, float z,float width,float wallHeight,int[] sidesRequired,int maxSides, GameObject[] spawnables, bool enemy, bool friendly, string name,GameObject mainObject)
     {
         _position[0] = x;
         _position[1] = z;
@@ -228,15 +230,14 @@ public class pathObject
         {
             _floor = new GameObject();
             _floor.transform.position = new Vector3(x, 0, z);
-            _floor.name = row.ToString() + " " + column.ToString();
+            _floor.name = name;
             UnityEngine.AI.NavMeshObstacle tempObstacle;
+            _floor.transform.parent = mainObject.transform;
+            generateSpawnables(_floor,spawnables, enemy, friendly, new float[2] { x - width / 6, x + width / 6 }, new float[2] { z - width / 6, z + width / 6 });
 
             if (_sides[2] == true)
             {
                 _path1 = new GameObject[4];
-                //_path1[0] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                //_path1[0].transform.position = new Vector3(x + width / 3, 0, z);
-                //_path1[0].transform.localScale = new Vector3(width / 3, 1, width / 3);
                 _path1[0] = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 _path1[0].transform.position = new Vector3(x + width / 3, wallHeight/2, z + width / 6 + 0.5f);
                 _path1[0].transform.localScale = new Vector3(width / 3, wallHeight, 1);
@@ -247,11 +248,14 @@ public class pathObject
                 _path1[1].transform.localScale = new Vector3(width / 3, wallHeight, 1);
                 tempObstacle = _path1[1].AddComponent(typeof(UnityEngine.AI.NavMeshObstacle)) as UnityEngine.AI.NavMeshObstacle;
                 tempObstacle.carving = true;
+                _path1[3] = new GameObject("Path1");
+                _path1[3].transform.position = new Vector3(x + width / 3, 0, z);
+                _path1[3].transform.parent = _floor.transform;
+                _path1[0].transform.parent = _path1[3].transform;
+                _path1[1].transform.parent = _path1[3].transform;
                 //_path1[3] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                //_path1[0].transform.parent = _floor.transform;
-                _path1[0].transform.parent = _floor.transform;
-                _path1[1].transform.parent = _floor.transform;
                 //_path1[3].transform.parent = _floor.transform;
+                generateSpawnables(_path1[3], spawnables, enemy, friendly, new float[2] { x + width / 6, x + width / 2}, new float[2] { z - width / 6 + 1f, z + width / 6 - 1f });
 
             }
             else
@@ -269,9 +273,6 @@ public class pathObject
             if (_sides[3] == true)
             {
                 _path2 = new GameObject[4];
-               // _path2[0] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-               // _path2[0].transform.position = new Vector3(x - width / 3, 0, z);
-               // _path2[0].transform.localScale = new Vector3(width / 3, 1, width / 3);
                 _path2[0] = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 _path2[0].transform.position = new Vector3(x - width / 3, wallHeight / 2, z + width / 6 + 0.5f);
                 _path2[0].transform.localScale = new Vector3(width / 3, wallHeight, 1);
@@ -282,11 +283,14 @@ public class pathObject
                 _path2[1].transform.localScale = new Vector3(width / 3, wallHeight, 1);
                 tempObstacle = _path2[1].AddComponent(typeof(UnityEngine.AI.NavMeshObstacle)) as UnityEngine.AI.NavMeshObstacle;
                 tempObstacle.carving = true;
+                _path2[3] = new GameObject("Path2");
+                _path2[3].transform.position = new Vector3(x - width / 3, 0, z);
+                _path2[3].transform.parent = _floor.transform;
                 //_path2[3] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                //_path2[0].transform.parent = _floor.transform;
-                _path2[0].transform.parent = _floor.transform;
-                _path2[1].transform.parent = _floor.transform;
-               // _path2[3].transform.parent = _floor.transform;
+                _path2[0].transform.parent = _path2[3].transform;
+                _path2[1].transform.parent = _path2[3].transform;
+                // _path2[3].transform.parent = _floor.transform;
+                generateSpawnables(_path2[3], spawnables, enemy, friendly, new float[2] { x - width / 2, x - width / 6 }, new float[2] { z - width / 6 + 1f, z + width / 6 - 1f });
 
             }
             else
@@ -304,9 +308,6 @@ public class pathObject
             if (_sides[0] == true)
             {
                 _path3 = new GameObject[4];
-                //_path3[0] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                //_path3[0].transform.position = new Vector3(x, 0, z + width / 3);
-                //_path3[0].transform.localScale = new Vector3(width / 3, 1, width / 3);
                 _path3[0] = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 _path3[0].transform.position = new Vector3(x + width / 6 + 0.5f, wallHeight / 2, z + width / 3);
                 _path3[0].transform.localScale = new Vector3(1, wallHeight, width / 3);
@@ -317,11 +318,14 @@ public class pathObject
                 _path3[1].transform.localScale = new Vector3(1, wallHeight, width / 3);
                 tempObstacle = _path3[1].AddComponent(typeof(UnityEngine.AI.NavMeshObstacle)) as UnityEngine.AI.NavMeshObstacle;
                 tempObstacle.carving = true;
+                _path3[3] = new GameObject("Path3");
+                _path3[3].transform.position = new Vector3(x, 0, z + width / 3);
+                _path3[3].transform.parent = _floor.transform;
                 // _path3[3] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                // _path3[0].transform.parent = _floor.transform;
-                _path3[0].transform.parent = _floor.transform;
-                _path3[1].transform.parent = _floor.transform;
+                _path3[0].transform.parent = _path3[3].transform;
+                _path3[1].transform.parent = _path3[3].transform;
                 //_path3[3].transform.parent = _floor.transform;
+                generateSpawnables(_path3[3], spawnables, enemy, friendly, new float[2] { x - width / 6 + 1f, x + width / 6 - 1f },new float[2] { z + width / 6, z + width / 2 });
 
             }
             else
@@ -339,9 +343,6 @@ public class pathObject
             if (_sides[1] == true)
             {
                 _path4 = new GameObject[4];
-                //_path4[0] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                //_path4[0].transform.position = new Vector3(x, 0, z - width / 3);
-                //_path4[0].transform.localScale = new Vector3(width / 3, 1, width / 3);
                 _path4[0] = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 _path4[0].transform.position = new Vector3(x + width / 6 + 0.5f, wallHeight / 2, z - width / 3);
                 _path4[0].transform.localScale = new Vector3(1, wallHeight, width / 3);
@@ -352,11 +353,14 @@ public class pathObject
                 _path4[1].transform.localScale = new Vector3(1, wallHeight, width / 3);
                 tempObstacle = _path4[1].AddComponent(typeof(UnityEngine.AI.NavMeshObstacle)) as UnityEngine.AI.NavMeshObstacle;
                 tempObstacle.carving = true;
+                _path4[3] = new GameObject("Path4");
+                _path4[3].transform.position = new Vector3(x, 0, z - width / 3);
+                _path4[3].transform.parent = _floor.transform;
                 //_path4[3] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                //_path4[0].transform.parent = _floor.transform;
-                _path4[0].transform.parent = _floor.transform;
-                _path4[1].transform.parent = _floor.transform;
+                _path4[0].transform.parent = _path4[3].transform;
+                _path4[1].transform.parent = _path4[3].transform;
                 //_path4[3].transform.parent = _floor.transform;
+                generateSpawnables(_path4[3], spawnables, enemy, friendly, new float[2] { x - width / 6 + 1f, x + width / 6 - 1f}, new float[2] { z - width / 6, z - width / 2 });
 
             }
             else
@@ -372,6 +376,14 @@ public class pathObject
                 _path4[1].transform.parent = _floor.transform;
             }
         }
+        int _neededSides = 0;
+        for(int i = 0; i < _sides.Length; i++)
+        {
+            if (sidesRequired[i] == 2)
+            {
+                _neededSides += 1;
+            }
+        }
         return sideCount;
 
     }
@@ -379,6 +391,48 @@ public class pathObject
     public float[] position()
     {
         return _position;
+    }
+    public void setHomeTree()
+    {
+        //make home
+    }
+    public void generateSpawnables(GameObject parent,GameObject[] spawnables,bool enemy,bool friendly,float[] xLimits,float[] zLimits)
+    {
+        float chance = UnityEngine.Random.value;
+        float xT;
+        float zT;
+        if(chance > 0.66)
+        {
+            pathType = "Terrain";
+        }else
+        {
+            if(enemy == true & chance > 0.33)
+            {
+                pathType = "Enemy";
+            }else if(friendly == true)
+            {
+                pathType = "Friendly";
+            }else
+            {
+                pathType = "Terrain";
+            }
+        }
+        for (int spawnArray = 0;spawnArray < spawnables.Length;spawnArray++)
+        {
+            objectData current = spawnables[spawnArray].GetComponent<objectData>();
+            
+            if (current.objectType == "Terrain" || current.objectType == pathType)
+            {
+                for(int objects = 0; objects < Random.Range(0.0f, current.maxAmount); objects++)
+                {
+                    xT = UnityEngine.Random.Range((xLimits[1] - xLimits[0]) / 2.0f * current.minPercentageFromSideX / 100f, (xLimits[1] - xLimits[0]) / 2.0f * current.maxPercentageFromSideX / 100f);
+                    zT = UnityEngine.Random.Range((zLimits[1] - zLimits[0]) / 2.0f * current.minPercentageFromSideZ / 100f, (zLimits[1] - zLimits[0]) / 2.0f * current.maxPercentageFromSideZ / 100f);
+                    Object.Instantiate(spawnables[spawnArray], parent.transform.position + new Vector3(xT * RandomSign(), 0, zT * RandomSign()), Quaternion.identity, parent.transform);
+                    //Debug.Log(((xLimits[1] - xLimits[0]) / 2.0f).ToString() +" "+ xT.ToString() + " " + parent.name);
+                }
+            }
+            
+        }
     }
     public int sideNeeded(int side)
     {
@@ -389,4 +443,17 @@ public class pathObject
             return 0;
         }
     }
+    public int RandomSign()
+    {
+        if (Random.value < 0.5)
+        {
+            return -1;
+        }
+        else
+        {
+            return 1;
+        }
+    }
 }
+
+
---
+++ GES_Assignment_Connor/Assets/Scripts/objectData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectData : MonoBehaviour
+{
+    public float minPercentageFromSideX = 0;
+    public float maxPercentageFromSideX = 100;
+    public float minPercentageFromSideZ = 0;
+    public float maxPercentageFromSideZ = 100;
+    public int maxAmount = 3;
+    public string objectType = "Terrain";
+    public float[] objectScale = new float[3] { 1, 1, 1 };
+    GameObject gameObject;
+
+    void Start()
+    {
+        if (objectType != "Enemy" || objectType != "Friendly")
+        {
+            transform.localScale = new Vector3(objectScale[0], objectScale[1], objectScale[2]);
+        }
+
+    }
+}
